981ff3975ced75f317023983f9f7e5f4696942ef
liujiawen <liujiawen20202020@163.com>
2023-03-20 17:28:09 +0800

    coding the base structure of the Login component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import './Login.less'
+import logo from './images/logo.png'
 // 登陆的路由组件
 export default class Login extends Component {
     render() {
         return (
-            <div>Login</div>
+            <div className="login">
+                <header className="login-header">
+                     <img src={logo} />
+                    <h1>React项目: 后台管理系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登陆</h2>
+                    <div>Form</div>
+                </section>
+            </div>
         )
     }
 }
