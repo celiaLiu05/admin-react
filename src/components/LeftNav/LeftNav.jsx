5b0a22e592a5fda3fb842847b14379f1b6036fb7
liujiawen <liujiawen20202020@163.com>
2023-03-30 15:58:42 +0800

    complete the static component of LeftNav

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -1,9 +1,47 @@
 import React, {Component} from "react"
+import { Link } from "react-router-dom"
+import { Menu, Icon, Button } from 'antd';
 import './LeftNav.less'
+import logo from '../../assets/images/logo.png'
+
+const { SubMenu } = Menu;
+
 export default class LeftNav extends Component {
     render() {
         return(
-            <div className="leftNav">LeftNav</div>
+            <div className="left-nav">
+                <Link to="/" className="left-nav-header">
+                    <img src={logo} alt="logo" />
+                    <h1>硅谷后台</h1>
+                </Link>
+                <Menu
+                    mode="inline"
+                    theme="dark"
+                    >
+                    <Menu.Item key="1">
+                        <Icon type="pie-chart" />
+                        <span>首页</span>
+                    </Menu.Item>
+                    <SubMenu
+                        key="sub1"
+                        title={
+                        <span>
+                            <Icon type="mail" />
+                            <span>商品</span>
+                        </span>
+                        }
+                    >
+                        <Menu.Item key="5">
+                            <Icon type="mail" />
+                            <span>品类管理</span>
+                        </Menu.Item>
+                        <Menu.Item key="6">
+                            <Icon type="mail" />
+                            <span>商品管理</span>
+                        </Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </div>
         )
     }
 }
