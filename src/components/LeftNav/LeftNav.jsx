aa745a911b5a0a0bbbcab981274a0771c37e37e0
liujiawen <liujiawen20202020@163.com>
2023-03-30 09:33:40 +0800

    base structure of Admin

---
+++ src/components/LeftNav/LeftNav.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react"
+import './LeftNav.less'
+export default class LeftNav extends Component {
+    render() {
+        return(
+            <div className="leftNav">LeftNav</div>
+        )
+    }
+}
--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react"
 import { Redirect } from "react-router-dom"
+import { Layout } from 'antd';
 import memoryUtils from "../../utils/memoryUtils"
+import LeftNav from "../../components/LeftNav/LeftNav";
+import Header from "../../components/Header/Header";
+
+const { Footer, Sider, Content } = Layout;
 // 后台管理的路由组件
 export default class Admin extends Component {
     render() {
         const user = memoryUtils.user
-        // 如果内存中没有存储user ==> 当前没有登陆
+        // 如果内存中没有存储user == > 当前没有登陆
         if(!user || !user._id) {
             // 自动跳转到登陆（在render()中）
             return <Redirect to="/login"/>
         }
         return (
-            <div>aaa{user.username}</div>
+            <Layout style={{height: '100%'}}>
+                <Sider>
+                    <LeftNav/>
+                </Sider>
+                <Layout>
+                    <Header/>
+                    <Content style={{backgroundColor: "#fff"}}>Content</Content>
+                    <Footer style={{textAlign: 'center', color: '#ccc '}}>推荐使用谷歌浏览器，可以获得更佳页面操作体验</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
