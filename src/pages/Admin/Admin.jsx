51e2a763c5e89bddc72aa9dd850de9f025eb88ff
liujiawen <liujiawen20202020@163.com>
2023-04-01 20:41:55 +0800

    Static interface of Header

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -30,7 +30,7 @@ export default class Admin extends Component {
                 </Sider>
                 <Layout>
                     <Header/>
-                    <Content style={{backgroundColor: "#fff"}}>
+                    <Content style={{backgroundColor: "#fff", margin: '20px'}}>
                         <Switch>
                             <Route path='/home' component={Home} />
                             <Route path='/category' component={Category} />
