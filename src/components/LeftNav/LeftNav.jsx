bbfb523fd0f1b19c9bb4f3ad04b3aedd970f1779
liujiawen <liujiawen20202020@163.com>
2023-04-01 14:55:20 +0800

    Dynamic display of menu lists(reduce& recursion)

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -11,35 +11,67 @@ const { SubMenu } = Menu;
 
 export default class LeftNav extends Component {
     // 根据menu的数据数组生成对应的标签数组
-    // 使用用map + 递归调用
-    getMenuNodes = (menuList) => {
-        return menuList.map((item) => {
-            if(!item.children) {
-                return (
-                    <Menu.Item key={item.key}>
-                        <Link to={item.key}>
-                            <Icon type={item.icon} />
-                            <span>{item.title}</span>
-                        </Link>
-                    </Menu.Item>
-                )
-            }else {
-                return(
-                    <SubMenu
-                        key={item.key}
-                        title={
-                        <span>
-                            <Icon type={item.icon} />
-                            <span>{item.title}</span>
-                        </span>
-                        }
-                    >
-                        { this.getMenuNodes(item.children) }
-                    </SubMenu>
-                )
-            }
-        })
-    }
+    // 使用map + 递归调用
+    // getMenuNodes_map = (menuList) => {
+    //     return menuList.map((item) => {
+    //         if(!item.children) {
+    //             return (
+    //                 <Menu.Item key={item.key}>
+    //                     <Link to={item.key}>
+    //                         <Icon type={item.icon} />
+    //                         <span>{item.title}</span>
+    //                     </Link>
+    //                 </Menu.Item>
+    //             )
+    //         }else {
+    //             return(
+    //                 <SubMenu
+    //                     key={item.key}
+    //                     title={
+    //                     <span>
+    //                         <Icon type={item.icon} />
+    //                         <span>{item.title}</span>
+    //                     </span>
+    //                     }
+    //                 >
+    //                     { this.getMenuNodes(item.children) }
+    //                 </SubMenu>
+    //             )
+    //         }
+    //     })
+    // }
+
+    // 根据menu的数据数组生成对应的标签数组
+    // 使用用reduce + 递归调用
+        getMenuNodes = (menuList) => {
+            return menuList.reduce((pre, item) => {
+                if(!item.children) {
+                    pre.push((
+                        <Menu.Item key={item.key}>
+                            <Link to={item.key}>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </Link>
+                        </Menu.Item>
+                    ))
+                }else {
+                    pre.push((
+                        <SubMenu
+                            key={item.key}
+                            title={
+                            <span>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                            }
+                        >
+                            { this.getMenuNodes(item.children) }
+                        </SubMenu>
+                    ))
+                }
+                return pre
+            }, [])
+        }
     render() {
         return(
             <div className="left-nav">
