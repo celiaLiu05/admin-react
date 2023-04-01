ddea8c5e76d67fa8bed2e1596cf5f5132872784a
liujiawen <liujiawen20202020@163.com>
2023-04-01 15:17:20 +0800

    select the current menu automatically of LeftNav

--- src/components/LeftNav/LeftNav.jsx
+++ src/components/LeftNav/LeftNav.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import { Link } from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 import { Menu, Icon } from 'antd'
 import './LeftNav.less'
 import logo from '../../assets/images/logo.png'
@@ -9,7 +9,7 @@ const { SubMenu } = Menu;
 
 // 左侧导航的组件
 
-export default class LeftNav extends Component {
+class LeftNav extends Component {
     // 根据menu的数据数组生成对应的标签数组
     // 使用map + 递归调用
     // getMenuNodes_map = (menuList) => {
@@ -43,36 +43,38 @@ export default class LeftNav extends Component {
 
     // 根据menu的数据数组生成对应的标签数组
     // 使用用reduce + 递归调用
-        getMenuNodes = (menuList) => {
-            return menuList.reduce((pre, item) => {
-                if(!item.children) {
-                    pre.push((
-                        <Menu.Item key={item.key}>
-                            <Link to={item.key}>
-                                <Icon type={item.icon} />
-                                <span>{item.title}</span>
-                            </Link>
-                        </Menu.Item>
-                    ))
-                }else {
-                    pre.push((
-                        <SubMenu
-                            key={item.key}
-                            title={
-                            <span>
-                                <Icon type={item.icon} />
-                                <span>{item.title}</span>
-                            </span>
-                            }
-                        >
-                            { this.getMenuNodes(item.children) }
-                        </SubMenu>
-                    ))
-                }
-                return pre
-            }, [])
-        }
+    getMenuNodes = (menuList) => {
+        return menuList.reduce((pre, item) => {
+            if(!item.children) {
+                pre.push((
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon} />
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                ))
+            }else {
+                pre.push((
+                    <SubMenu
+                        key={item.key}
+                        title={
+                        <span>
+                            <Icon type={item.icon} />
+                            <span>{item.title}</span>
+                        </span>
+                        }
+                    >
+                        { this.getMenuNodes(item.children) }
+                    </SubMenu>
+                ))
+            }
+            return pre
+        }, [])
+    }
     render() {
+        //  得到当前的请求的路由路径
+        const path = this.props.location.pathname
         return(
             <div className="left-nav">
                 <Link to="/" className="left-nav-header">
@@ -82,6 +84,7 @@ export default class LeftNav extends Component {
                 <Menu
                     mode="inline"
                     theme="dark"
+                    selectedKeys={[path]}
                 >
                         { this.getMenuNodes(menuList) }
                 </Menu>
@@ -89,3 +92,8 @@ export default class LeftNav extends Component {
         )
     }
 }
+
+// withRouter 高阶组件：
+// 包装非路由组件，返回一个新的组件
+// 新的组件向非路由组件传递三个属性：history/location/match
+export default withRouter(LeftNav)
