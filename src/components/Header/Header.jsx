e9fe528350b39820d8d70885c1a2baf4e2ed8588
liujiawen <liujiawen20202020@163.com>
2023-04-04 09:17:20 +0800

    Dynamically displays the current time and title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,61 @@
 import React, {Component} from "react"
+import { withRouter } from "react-router-dom"
+import {formateDate} from '../../utils/dateUtils'
+import memoryUtils from "../../utils/memoryUtils"
+import menuList from "../../config/menuConfig"
 import './Header.less'
-export default class Header extends Component {
+class Header extends Component {
+    state = {
+        currentTime: formateDate(Date.now())
+    }
+
+    getTime = () => {
+        // 每隔一秒获取当前时间，并更新状态数据currentTime
+        setInterval(() => {
+            const currentTime = formateDate(Date.now())
+            this.setState({currentTime})
+        }, 1000)
+    }
+
+    getTitle = () => {
+        // 得到当前请求路径
+        const path = this.props.location.pathname
+        let title
+        menuList.forEach((item) => {
+            if(item.key === path) { // 如果当前item对象的key和path一样，item的title就是需要显示的title
+                title = item.title
+            }else if(item.children) {
+                // 在所有的子item中查找匹配的
+                const cItem = item.children.find((cItem) => cItem.key === path)
+                // 如果有值说明有匹配的
+                if(cItem) {
+                    // 取出它的title
+                    title = cItem.title
+                }
+            }
+        })
+        return title
+    }
+    // 在第一次render之后执行一次
+    // 一般在此执行异步操作：发ajax请求/启动定时器
+    componentDidMount() {
+        // 获取当前时间
+        this.getTime()
+    }
     render() {
+        const {currentTime} = this.state
+        const username = memoryUtils.user.username
+        const title = this.getTitle()
         return(
             <div className="header">
                 <div className="header-top">
-                    <span>欢迎，admin</span>
+                    <span>欢迎，{username}</span>
                     <a href="javascript:;">退出</a>
                 </div>
                 <div className="header-bottom">
-                    <div className="header-bottom-left">
-                        首页
-                    </div>
+                    <div className="header-bottom-left">{title}</div>
                     <div className="header-bottom-right">
-                        <span>2019-5-16 10:12:34</span>
+                        <span>{currentTime}</span>
                         <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather" />
                         <span>晴</span>
                     </div>
@@ -22,3 +64,5 @@ export default class Header extends Component {
         )
     }
 }
+
+export default withRouter(Header)
---
+++ src/utils/dateUtils.js
@@ -0,0 +1,6 @@
+// 格式化时间
+export function formateDate(time) {
+    if(!time) return ''
+    let date = new Date(time)
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDay() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
+}
