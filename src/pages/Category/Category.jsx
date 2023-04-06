0d4ce8ccd25ef0ce8e4966a17eb1ee9182ee5c6c
liujiawen <liujiawen20202020@163.com>
2023-04-06 09:01:49 +0800

    Asynchronously displays a list of first-level

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,32 +1,17 @@
-import React, {Component} from "react";
-import {Card, Table, Button, Icon} from 'antd'
+import React, {Component} from "react"
+import {reqCategorys} from '../../api'
+import {Card, Table, Button, Icon, message} from 'antd'
 import LinkButton from "../../components/LinkButton/LinkButton";
 
 export default class Category extends Component {
-    render() {
-        const title = '一级分类列表'
-        const extra = (
-            <Button type="primary">
-                <Icon type="plus"></Icon>
-                添加
-            </Button>
-        )
-        const dataSource = [
-            {
-                "parentId": "0",
-                "_id": "5e12b8bce31bb727e4b0e348",
-                "name": "家用电器",
-                "__v": 0
-            },
-            {
-                "parentId": "0",
-                "_id": "5e130ec7e31bb727e4b0e34c",
-                "name": "洗衣机",
-                "__v": 0
-            }
-        ]
-          
-          const columns = [
+    state = {
+        categorys: [], // 一级分类列表 
+        loading: false // 是否正在获取数据中 
+    }
+
+    // 初始化Table所有列的数组
+    initColumns = () => {
+        this.columns = [
             {
               title: '分类名称',
               dataIndex: 'name', // 显示数据对应的属性名  
@@ -39,18 +24,57 @@ export default class Category extends Component {
                     <LinkButton>修改分类</LinkButton>
                     <LinkButton>查看子分类</LinkButton>
                 </div>
-              ),
+              )
             }
           ]
+    }
+
+    // 异步获取一级分类列表显示
+    getCategorys = async() => {
+        // 在发请求前，显示loading
+        this.setState({loading: true})
+        // 发异步ajax请求， 获取数据
+        const result = await reqCategorys("0")
+        // 在请求完成后，隐藏loading
+        this.setState({loading: false})
+        if(result.status === 0) {
+            const categorys = result.data
+            // 更新状态
+            this.setState({categorys})
+        }else {
+            message.error('获取分类列表失败！')
+        }
+    }
+
+    // 为第一次render准备数据
+    componentWillMount() {
+        this.initColumns()
+    }
+
+    // 执行异步任务： 发异步ajax请求
+    componentDidMount() {
+        this.getCategorys()
+    }
+    render() {
+        const {categorys, loading} = this.state
+        const title = '一级分类列表'
+        const extra = (
+            <Button type="primary">
+                <Icon type="plus"></Icon>
+                添加
+            </Button>
+        )
         return (
             <div>
                 <Card title={title} extra={extra}>
                     <Table 
-                        dataSource={dataSource} 
-                        columns={columns} 
                         bordered
                         rowKey="_id"
-                    />;
+                        loading={loading}
+                        dataSource={categorys} 
+                        columns={this.columns} 
+                        pagination={{defaultPageSize: 5, showQuickJumper: true}}
+                    />
                 </Card>
             </div>
         )
