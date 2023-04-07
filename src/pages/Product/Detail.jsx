91b1af1db1df3296077fc1d4c001207217ff847f
liujiawen <liujiawen20202020@163.com>
2023-04-07 20:19:21 +0800

    Dynamic display of product information

--- src/pages/Product/Detail.jsx
+++ src/pages/Product/Detail.jsx
@@ -1,30 +1,41 @@
 import React, {Component} from "react"
 import {Card, Icon, List} from 'antd'
-
+import LinkButton from "../../components/LinkButton/LinkButton"
 const Item = List.Item
 // product的详情子路由组件
 export default class Detail extends Component {
+
     render() {
         const title = (
             <span>
-                <Icon type="arrow-left"/>
+                <LinkButton>
+                    <Icon 
+                        type="arrow-left" 
+                        style={{marginRight: 10, fontSize: 18}}
+                        onClick={() => this.props.history.goBack()}
+                    />
+                </LinkButton>
                 <span>商品详情 </span>
             </span>
         )
+        
+        // 读取携带过来的state数据
+        const {name, desc, price, imgs, detail} = this.props.location.state.product
+        
         return(
             <Card title={title} className='product-detail'>
                 <List>
                     <Item>
                         <span className='left'>商品名称：</span>
-                        <span>aaaaaaaaaaaaaa</span>
+                        <span>{name}</span>
                     </Item>
                     <Item>
                         <span className='left'>商品描述：</span>
-                        <span>aaaaaaaaaaaaaa</span>
+                        <span>{desc}</span>
                     </Item>
                     <Item>
                         <span className='left'>商品价格：</span>
-                        <span>aaaaaaaaaaaaaa</span>
+                        <span>{price}元</span>
                     </Item>
                     <Item>
                         <span className='left'>所属分类：</span>
@@ -39,7 +50,7 @@ export default class Detail extends Component {
                     </Item>
                     <Item>
                         <span className='left'>商品详情：</span>
-                        <span dangerouslySetInnerHTML={{__html: '<h1 style="color: red">aaa</h1>'}}></span>
+                        <span dangerouslySetInnerHTML={{__html: detail}}></span>
                     </Item>
                 </List>
             </Card>
