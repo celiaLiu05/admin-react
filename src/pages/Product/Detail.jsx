02eec18fc1cee842b147f08d19dc296267f20aa5
liujiawen <liujiawen20202020@163.com>
2023-04-07 09:10:03 +0800

    Build the overall routing of the product

---
+++ src/pages/Product/Detail.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+
+// product的详情子路由组件
+export default class Detail extends Component {
+    render() {
+        return(
+            <div>Detail</div>
+        )
+    }
+}
--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
+import { Route, Switch, Redirect } from "react-router-dom"
+import ProductHome from "./ProductHome"
+import ProductAddUpdate from "./ProductAddUpdate"
+import Detail from "./Detail"
 
 export default class Product extends Component {
     render() {
         return (
             <div>
-                Product
+                <Switch>
+                    {/* 路径完全匹配 */}
+                    <Route path='/product' component={ProductHome} exact/> 
+                    <Route path='/product/addupdate' component={ProductAddUpdate}/>
+                    <Route path='/product/detail' component={Detail}/>
+                    <Redirect to='/product' />
+                </Switch>
             </div>
         )
     }
